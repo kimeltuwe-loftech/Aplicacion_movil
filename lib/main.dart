07c6b4e261289561a83393aaca474c29b29ce5bd
valentijnvanwinden <valentijnvanwinden@gmail.com>
2025-11-19 16:55:42 +0100

    Change layout of buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'sensores.dart';
 import 'ficha.dart';
-void main() =>  runApp(const MyApp());
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -16,93 +17,117 @@ class MyApp extends StatelessWidget {
         backgroundColor: const Color(0xFFD0EAFF),
         body: ListView(
           children: [
-            Center ( child: 
-                  Container(
-                    width: 240,
-                    height: 150,
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(250),
-                        bottomRight: Radius.circular(250),
-                      )
-                    ),
-                    child: Center(
-                      child: Image.asset(
-                    'assets/logo_principal.png', 
-                    width: 120, 
+            Center(
+              child: Container(
+                width: 240,
+                height: 150,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(250),
+                    bottomRight: Radius.circular(250),
+                  ),
+                ),
+                child: Center(
+                  child: Image.asset(
+                    'assets/logo_principal.png',
+                    width: 120,
                     height: 120,
                   ),
-                      )
-                  ),
-                  ),          
-            const SizedBox(height: 50), 
-            Builder(
-                builder: (context) => Row(
-                mainAxisAlignment: MainAxisAlignment.center,    
-                children: [
-                ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Sensores()),
-                  );
-                },                
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF009900), 
-                  foregroundColor: Colors.white,
-                  padding:const EdgeInsets.symmetric(horizontal: 75, vertical: 20),
-                  textStyle:const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
                 ),
-                child: const Text('Sensores'),
-                ),
-                ],
-                ),
-                ),
-                const SizedBox(height: 30), // <-- Espacio de 10 píxeles entre los botones
-                Image.asset(
-                  'assets/sensores_principal.png', 
-                  width: 150, 
-                  height: 150,
-                ),
-                const SizedBox(height: 30),
-                 Builder(
-                  builder: (context) => Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+              ),
+            ),
+            const SizedBox(height: 50),
+
+            // New buttons
+            Center(
+              child: Builder(
+                builder: (context) => Container(
+                  width: 300,
+                  // color: Colors.red,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
                     children: [
                       ElevatedButton(
-                        onPressed: null,
-                        // onPressed: () {
-                        //   Navigator.push(
-                        //     context,
-                        //     MaterialPageRoute(builder: (context) => const FichasPlantas()),
-                        //   );
-                        // },
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Sensores(),
+                            ),
+                          );
+                        },
                         style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFF009900),
-                          foregroundColor: Colors.white,
-                          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                          textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 20,
+                          ),
+                          textStyle: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(20.0),
                           ),
-                          
                         ),
-                        child: const Text('Fichas de Plantas'),
-                        
+                        child: Row(
+                          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Image.asset(
+                              'assets/sensores_principal.png',
+                              width: 50,
+                              height: 50,
+                            ),
+                            SizedBox(width: 20),
+                            const Text('Sensores'),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const FichasPlantas(),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.black,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 20,
+                          ),
+                          textStyle: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              'assets/ficha.png',
+                              width: 50,
+                              height: 50,
+                            ),
+                            SizedBox(width: 20),
+                            const Text('Fichas de Plantas'),
+                          ],
+                        ),
                       ),
                     ],
                   ),
                 ),
-                const SizedBox(height: 30),
-                Image.asset(
-                  'assets/ficha.png', 
-                  width: 120, 
-                  height: 120,
-                ),
+              ),
+            ),
+
           ],
         ),
       ),
