d8adaf3fbd8f627ecf8a7cbc98a8c8802cb0994d
valentijnvanwinden <valentijnvanwinden@gmail.com>
2025-11-21 16:53:07 +0100

    Remove unused comment

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:provider/provider.dart';
 import 'sensores.dart';
 import 'ficha.dart';
 import 'l10n/app_localizations.dart';
-// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'util/udp.dart';
 
