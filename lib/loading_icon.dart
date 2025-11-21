b926bc790ccf637985de8ca25df8df9e2b77617a
valentijnvanwinden <valentijnvanwinden@gmail.com>
2025-11-21 18:56:28 +0100

    Remove unused package

--- lib/loading_icon.dart
+++ lib/loading_icon.dart
@@ -1,6 +1,5 @@
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class SpinningLoaderIcon extends StatefulWidget {
   final String assetName;
